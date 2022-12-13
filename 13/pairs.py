7366041b7cce900d2cef94aac701f202191ce862
mthornba <matt.thornback@gmail.com>
2022-12-13 16:41:59 -0500

    day 13

--- 13/pairs.py
+++ 13/pairs.py
@@ -13,12 +13,30 @@ with open("sample") as f:
       pairs.append([])
       i=i+1
 
-print(pairs)
+print(pairs,'\n')
 
 pairs=tuple(tuple(map(ast.literal_eval, x)) for x in pairs)
+# pairs=[list(map(ast.literal_eval, x)) for x in pairs]
+
+# iterate over tuple
+  # iterate over lists
+    # elements can be both lists, both integers or 1 integer
 
 for pair in pairs:
   print(pair)
-
-# iterate over tuple
-  # iterate over elements of both lists, comparing them
+  for left,right in pairs:
+    zipped=zip(left,right)
+    # print(list(zipped))
+    for a,b in zipped:
+      # print(a,b)
+      if isinstance(a,int) and isinstance(b,int):
+        print('both integers')
+        if a <= b:
+          ordered=True
+        else:
+          ordered=False
+      elif isinstance(a,list) and isinstance(b,list):
+        print('both lists')
+      else:
+        print('one integer')
+    print(ordered)
