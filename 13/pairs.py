e18b8b7bf1acf49e995a2d1c42ab67051703371a
mthornba <matt.thornback@gmail.com>
2022-12-13 14:10:52 -0500

    day 13

--- 13/pairs.py
+++ 13/pairs.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 import ast
+import itertools
+from itertools import groupby
 
 # read file as a tuple of tuples of lists
+
+pairs=[[]]
+i=0
 with open("sample") as f:
-  pairs = [line.strip() for line in f]
+  for line in f:
+    if line.strip():
+      pairs[i].append(line.strip())
+    else:
+      pairs.append([])
+      i=i+1
 
 print(pairs)
