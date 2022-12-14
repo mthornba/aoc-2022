67e191e85e2ffbc018d4f8ccb2d8f612ef9aa2c7
mthornba <matt.thornback@gmail.com>
2022-12-13 19:01:53 -0500

    day 13

--- 13/pairs.py
+++ 13/pairs.py
@@ -22,21 +22,30 @@ pairs=tuple(tuple(map(ast.literal_eval, x)) for x in pairs)
   # iterate over lists
     # elements can be both lists, both integers or 1 integer
 
-for pair in pairs:
-  print(pair)
-  for left,right in pairs:
-    zipped=zip(left,right)
-    # print(list(zipped))
-    for a,b in zipped:
-      # print(a,b)
-      if isinstance(a,int) and isinstance(b,int):
-        print('both integers')
-        if a <= b:
-          ordered=True
-        else:
-          ordered=False
-      elif isinstance(a,list) and isinstance(b,list):
-        print('both lists')
+
+def zipcomp(l,r):
+  print(l,r)
+  zipped=zip(l,r)
+  # print(list(zipped))
+  for a,b in zipped:
+    print(a,b)
+    if isinstance(a,int) and isinstance(b,int):
+      # print('both integers')
+      if a <= b:
+        ordered=True
       else:
-        print('one integer')
-    print(ordered)
+        ordered=False
+        print('out of order')
+        return
+  #   elif isinstance(a,list) and isinstance(b,list):
+  #     print('both lists')
+  #   else:
+  #     print('one integer')
+  # print(ordered)
+
+# ordered=False
+for left,right in pairs:
+  if isinstance(left,list) and isinstance(right,list):
+    zipcomp(left,right)
+    # print(ordered)
+    break
