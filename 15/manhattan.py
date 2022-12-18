9415c6237a9879a935de8691189e2347ffca013f
mthornba <matt.thornback@gmail.com>
2022-12-18 11:15:25 -0500

    day 15: part 2

--- 15/manhattan.py
+++ 15/manhattan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Optimized Bubble sort in Python
 from itertools import chain
+from itertools import tee
 import re
 import time
 start_time = time.monotonic_ns()
@@ -9,10 +10,11 @@ sensors = []
 beacons = []
 distance = []
 # Ty = 10
-TxRange = range(0)
 # filename = 'sample'
+# Dsquare = 20
 Ty = 2000000
 filename = 'input'
+Dsquare = 4000000
 
 with open(filename) as f:
   lines = filter(None, (line.rstrip() for line in f))
@@ -28,7 +30,7 @@ with open(filename) as f:
       beacons.append((int(match.group(1)),int(match.group(2))))
 
 # print(sensors)
-print(beacons)
+# print(set(beacons))
 num=len(sensors)
 
 # build array of manhattan distances
@@ -37,27 +39,38 @@ for i in range(num):
   (xb,yb) = beacons[i]
   distance.append(abs(xb - xs) + abs(yb - ys))
 
-print('Distances:',distance)
+# print('Distances:',distance)
 
 # build list of coords within manhattan distance for each sensor
 
-for i in range(num):  # num of sensors
-# for i in range(7):  # num of sensors
-  print('Working on Sensor',i+1)
-  M = distance[i]          # Manhattan distance for Sensor i
-  (Sx,Sy)=sensors[i]       # get coords of Sensor i
-  D = abs(Ty - Sy)              # distance to Target y (Ty)
-  # print('M =',M)
-  # print('D =',D)
-  if M >= D:
-    TxRange = chain(TxRange, range(Sx+D-M,Sx+M-D+1))
-    # print(set(range(Sx+D-M,Sx+M-D+1)))
+for Ty in range(Dsquare+1):
+  if (Ty)%1000 == 0: print('Working on Y',Ty)
+  TxRange = range(0)
+  for i in range(num):  # num of sensors
+  # for i in range(7):  # num of sensors
+    # print('Sensor',i+1)
+    M = distance[i]          # Manhattan distance for Sensor i
+    (Sx,Sy)=sensors[i]       # get coords of Sensor i
+    D = abs(Ty - Sy)              # distance to Target y (Ty)
+    # if (i == 3 or i == 9) and Ty == 11:
+    #   print('Sensor:',i+1,'M:',M,'D:',D)
+    #   print('range:',range(max(Sx+D-M,0),min(Sx+M-D,Dsquare)+1))
+    if M >= D:
+      TxRange = chain(TxRange, range(max(Sx+D-M,0),min(Sx+M-D,Dsquare)+1))
 
-# find coords with y=2000000 (y=10 for sample)
+  # find coords with y=2000000 (y=10 for sample)
 
-# count beacons on target Y and subtract from ranges
-beaconCount = len({b for b in beacons if b[1] == Ty})
-# print(list(set(TxRange)))
-print(len(set(TxRange)) - beaconCount)
+  # count beacons on target Y and subtract from ranges
+  beaconCount = len({b for b in beacons if b[1] == Ty})
+  TxRange, TxRangeTest = tee(TxRange) # copy range to test length
+  TxRangeLen = len(set(TxRangeTest))
+  # print('Tlen:',TxRangeLen,'Dsq:',Dsquare)
+  if TxRangeLen < Dsquare+1:
+    print('x =',set(range(Dsquare+1)).difference(set(TxRange)))
+    print('y =',Ty)
+    # print(set(range(Dsquare+1)))
+    # print(set(TxRange))
+  # if (len(set(TxRange)) - beaconCount) == Dsquare: next
+  # else: print(Ty)
 
 print("--- %s microseconds ---" % ((time.monotonic_ns() - start_time)/1000))
