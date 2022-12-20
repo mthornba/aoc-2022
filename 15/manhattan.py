8488d5d6f0b2e816a4202311ad5f2ab9bb62effe
mthornba <matt.thornback@gmail.com>
2022-12-20 15:15:05 -0500

    day 15: part 2 - add matplot

--- 15/manhattan.py
+++ 15/manhattan.py
@@ -2,6 +2,7 @@
 # Optimized Bubble sort in Python
 from itertools import chain
 import shapely
+import matplotlib.pyplot as plt
 import re
 import time
 start_time = time.monotonic_ns()
@@ -18,6 +19,11 @@ Dsquare = 20
 # filename = 'input'
 # Dsquare = 4000000
 
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
 with open(filename) as f:
   lines = filter(None, (line.rstrip() for line in f))
   for line in lines:
@@ -32,7 +38,10 @@ with open(filename) as f:
       beacons.append((int(match.group(1)),int(match.group(2))))
 
 num=len(sensors)
+cmap = get_cmap(num)
 
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
 # build list of vertex coords
 for i in range(num):
   print('Working on Sensor',i)
@@ -42,7 +51,14 @@ for i in range(num):
   distance.append(M)
   vertices.append(((Sx-M,Sy),(Sx,Sy+M),(Sx+M,Sy),(Sx,Sy-M)))
   print(vertices[i])
-  polygons.append(shapely.Polygon(vertices[i]))
+  polygon = shapely.Polygon(vertices[i])
+  polygons.append(polygon)
+  print(polygon.exterior.xy)
+  x, y = polygon.exterior.xy
+  plt.plot([Sx],[Sy], c=cmap(i),marker="o")
+  plt.plot([Bx],[By], "g^")
+  plt.grid(True,which="both")
+  plt.plot(x, y, c=cmap(i))
 
 zone = shapely.box(0, 0, Dsquare, Dsquare)
 nobeacon = shapely.union_all(polygons,1)
@@ -52,3 +68,5 @@ tuningFreq = 4000000 * tuningX + tuningY
 print('Tuning Frequency:',tuningFreq)
 
 print("--- %s microseconds ---" % ((time.monotonic_ns() - start_time)/1000))
+
+plt.show()
